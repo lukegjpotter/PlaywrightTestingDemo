acfa4e54158c31b185e30fc794f34a5768e95fce
lukegjpotter <lukegjpotter@gmail.com>
2024-01-25 21:57:22 +0000

    Initial Commit

---
+++ src/test/java/com/lukegjpotter/playwright/MainTest.java
@@ -0,0 +1,13 @@
+package com.lukegjpotter.playwright;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MainTest {
+
+    @Test
+    void main() {
+        assertTrue(true);
+    }
+}
